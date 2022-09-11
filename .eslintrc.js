a2304a1dda1c8d30a13b5ce89029c2783a773fa9
ShawnL <45999699+shawnlin0201@users.noreply.github.com>
2022-09-12 00:22:47 +0800

    chore: 移除不必要的內容

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    node: true,
-  },
-  parser: 'vue-eslint-parser',
-  extends: ['eslint:recommended', 'plugin:vue/vue3-essential', 'plugin:prettier/recommended'],
-  rules: { 'prettier/prettier': ['error', { endOfLine: 'auto' }] },
-}
