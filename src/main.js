67b4e9d26f732355f1c304ac057a5749768fb28a
ShawnL <45999699+shawnlin0201@users.noreply.github.com>
2022-09-27 14:40:18 +0800

    chore: add unplug-in & markdown resolver

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import App from './App.vue'
 // styles
 import 'virtual:windi.css'
 import 'virtual:windi-devtools'
+import 'highlight.js/styles/github-dark-dimmed.css'
+import './styles/markdown.scss'
 
 const app = createApp(App)
 const pinia = createPinia()
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,45 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import vueComponent from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 import WindiCSS from 'vite-plugin-windicss'
 import eslintPlugin from 'vite-plugin-eslint'
+import markdown from 'vite-plugin-markdown'
+import markdownIt from 'markdown-it'
+import highlightjs from 'markdown-it-highlightjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
+  resolve: {
+    alias: {
+      '@/': new URL('./src/', import.meta.url).pathname,
+      vue: 'vue/dist/vue.esm-bundler.js',
+    },
+  },
   plugins: [
     vue(),
+    vueComponent({
+      resolvers: IconsResolver({
+        compiler: 'vue3',
+        prefix: 'i',
+      }),
+    }),
+    Icons(),
     eslintPlugin({
       include: ['src/**/*.js', 'src/**/*.vue', 'src/**/*.ts'],
     }),
     WindiCSS({
       fileExtensions: ['vue', 'js', 'html'],
+    }),
+    markdown.plugin({
+      mode: ['vue'],
+      markdownIt: markdownIt({
+        html: true,
+        linkify: true,
+        typographer: true,
+      }).use(highlightjs),
     }),
   ],
 })
