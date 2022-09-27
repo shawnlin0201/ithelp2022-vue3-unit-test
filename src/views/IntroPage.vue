114b14337ffba2be5470d701047321bd192f3b4b
ShawnL <45999699+shawnlin0201@users.noreply.github.com>
2022-09-27 14:41:16 +0800

    feat: add landing & intro page

--- src/views/IntroPage.vue
+++ src/views/IntroPage.vue
@@ -1,11 +1,38 @@
 <script setup>
-import BaseSection from '../components/BaseSection.vue'
+import { shallowRef, watch } from 'vue'
+import { useRoute } from 'vue-router'
+import router from '../router'
+
+const $route = useRoute()
+const pageComponent = shallowRef()
+const chapterPages = Object.keys(import.meta.glob('../views/intro/*.vue')).length
+
+const handleOnChangeView = async () => {
+  try {
+    pageComponent.value = (await import(`./intro/page-${$route.params.page}.vue`)).default
+  } catch (e) {
+    router.push('/')
+  }
+}
+
+watch(
+  () => $route.params.page,
+  async () => handleOnChangeView(),
+  {
+    immediate: true,
+  },
+)
 </script>
 
 <template>
   <div class="flex h-full justify-center items-center">
-    <BaseSection class="flex-col justify-center">
-      <h2 class="font-bold text-4xl">引導頁</h2>
+    <BaseSection class="min-w-200">
+      <div class="markdown-preview">
+        <component :is="pageComponent" />
+      </div>
+      <template v-slot:footer>
+        <BasePagination category="intro" :curr="+$route.params.page" :max="chapterPages"></BasePagination>
+      </template>
     </BaseSection>
   </div>
 </template>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,16 +1,21 @@
-<script setup>
-import BaseSection from '../components/BaseSection.vue'
-import BaseButton from '../components/BaseButton.vue'
-</script>
+<script setup></script>
 
 <template>
   <div class="flex h-full justify-center items-center">
-    <BaseSection class="flex flex-col items-center">
-      <h2 class="font-bold mb-4 text-2xl">歡迎來到 Vue3 Unit-Test！</h2>
-      <p class="mb-4">跟著系列文一起學習如何寫單元測試，試著將本專案加上單元測試吧！</p>
-      <BaseButton>
-        <router-link to="/intro">開始</router-link>
-      </BaseButton>
+    <BaseSection>
+      <div class="flex flex-col items-center">
+        <h2 class="font-bold mb-4 text-2xl">歡迎來到 Vue3 Unit-Test！</h2>
+        <p class="mb-4">
+          跟著
+          <a class="underline" href="https://ithelp.ithome.com.tw/users/20119062/ironman/5554" target="_blank"
+            >系列文</a
+          >
+          一起學習如何寫單元測試！
+        </p>
+        <router-link to="/intro/1?view=question">
+          <BaseButton>開始</BaseButton>
+        </router-link>
+      </div>
     </BaseSection>
   </div>
 </template>
