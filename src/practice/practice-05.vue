caa182cbf73033f50494167305979d93990a7ee8
ShawnL <45999699+shawnlin0201@users.noreply.github.com>
2022-10-10 19:42:51 +0800

    fix: adjust describe at chapter 3

--- src/practice/practice-05.vue
+++ src/practice/practice-05.vue
@@ -1,27 +1,17 @@
 <template>
-  <button @click="handleOnOpenAccount" data-test="button_open-account">開戶</button>
+  <button data-test="button_open-account">開戶</button>
   <input data-test="input_account" v-model="account" type="text" />
 
-  <button @click="handleOnDeposit" data-test="button_deposit">存款</button>
+  <button data-test="button_deposit">存款</button>
   <input data-test="input_deposit" v-model="deposit" type="number" min="0" />
 
-  <button @click="handleOnWithdraw" data-test="button_withdraw">提款</button>
+  <button data-test="button_withdraw">提款</button>
   <input data-test="input_withdraw" v-model="withdraw" type="number" min="0" />
 
   <p data-test="status">{{ status }}</p>
-  <p></p>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { FoodBank } from './practice-05.js'
-const account = ref('')
-const deposit = ref('')
-const withdraw = ref('')
-const status = ref('')
-
-const $FoodBank = new FoodBank()
-const handleOnOpenAccount = () => (status.value = $FoodBank.openAccount(account.value))
-const handleOnDeposit = () => (status.value = $FoodBank.deposit(account.value, deposit.value))
-const handleOnWithdraw = () => (status.value = $FoodBank.withdraw(account.value, withdraw.value))
+// import { ref } from 'vue'
+// import { FoodBank } from './practice-05.js'
 </script>
