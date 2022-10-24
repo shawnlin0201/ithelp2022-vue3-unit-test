46645de39aeccd50548d1b867f042fef52ab54ae
ShawnL <45999699+shawnlin0201@users.noreply.github.com>
2022-10-24 11:40:35 +0800

    feat: 調整第一個測試內容

--- src/practice/practice-01.spec.js
+++ src/practice/practice-01.spec.js
@@ -2,6 +2,9 @@ import { describe, expect, test } from 'vitest'
 
 describe.skip('你的第一個測試!', () => {
   test('1 + 1，應該為 2', () => {
-    expect(1 + 1).toBe(1)
+    expect(1 + 1).toBe(2)
+  })
+  test('1 + 1，應該為 2', () => {
+    expect(1 + 1).toBe(1) // 這個測試會失敗
   })
 })
